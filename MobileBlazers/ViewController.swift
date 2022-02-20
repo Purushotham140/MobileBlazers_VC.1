b0d883b35f8398c2bc2aad00a05816c8deba9736
Purushotham140 <purushothamj140@gmail.com>
2022-02-20 23:50:50 +0530

    function implemented

--- MobileBlazers/ViewController.swift
+++ MobileBlazers/ViewController.swift
@@ -14,9 +14,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        displayUIUI()
     }
 
-
+    func displayUIUI(){
+        print("upload")
+    }
 }
 
